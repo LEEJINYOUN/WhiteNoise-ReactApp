1e652574632eaa6db820957fa4923a297915e6cc
이진영 <swlsdud1005@naver.com>
2023-04-27 16:46:33 +0900

    데이터 카테고리별 필터링 기본 골격

--- src/routes/Lists.jsx
+++ src/routes/Lists.jsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import lists from "../data/lists.json";
 
 export default function Lists() {
-  return <div>리스트페이지</div>;
+  const [listsData, setListsData] = useState(lists);
+  const [filteredTitle, setFilteredTitle] = useState(lists);
+  const onClick = (category) => {
+    if (category === "전체") {
+      setFilteredTitle(lists);
+    } else {
+      setFilteredTitle(lists.filter((item) => item.category === category));
+    }
+  };
+  return (
+    <>
+      <div>
+        <ul>
+          {filteredTitle.map((item, key) => (
+            <li key={key}>{<h1>{item.title}</h1>}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {listsData.map((item, key) => (
+            <li key={key} onClick={() => onClick(item.category)}>
+              <button>{item.category}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
