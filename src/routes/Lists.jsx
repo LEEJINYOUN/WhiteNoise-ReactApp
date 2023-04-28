2a3dc4cc94e676467b57f4a00c955dcb34acd6f1
이진영 <swlsdud1005@naver.com>
2023-04-28 10:21:10 +0900

    리스트페이지 - 검색어를 클릭에서 검색으로 변경, 유튜브 api 연동

--- src/routes/Lists.jsx
+++ src/routes/Lists.jsx
@@ -1,34 +1,68 @@
 import React, { useState } from "react";
-import lists from "../data/lists.json";
+import searchJson from "../video/search.json";
+
+const words = ["파도", "풀벌레", "모닥불", "비"];
+
+// const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export default function Lists() {
-  const [listsData, setListsData] = useState(lists);
-  const [filteredTitle, setFilteredTitle] = useState(lists);
-  const onClick = (category) => {
-    if (category === "전체") {
-      setFilteredTitle(lists);
-    } else {
-      setFilteredTitle(lists.filter((item) => item.category === category));
+  const [youtubeData, setYoutubeData] = useState([]);
+  const getYoutubeData = async () => {
+    const fakeData = searchJson.items;
+    setYoutubeData(fakeData);
+    // const json = await (
+    //   await fetch(
+    //     `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${text}&key=${YOUTUBE_API_KEY}`
+    //   )
+    // ).json();
+    // setYoutubeData(json.items);
+  };
+
+  const [text, setText] = useState("");
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+  const onKeyUp = (e) => {
+    if (e.keyCode === 13) {
+      if (words.includes(text)) {
+        getYoutubeData();
+      } else {
+        console.log("잘못된 검색");
+      }
+      setText("");
     }
   };
+  console.log(youtubeData);
   return (
     <>
       <div>
-        <ul>
-          {filteredTitle.map((item, key) => (
-            <li key={key}>{<h1>{item.title}</h1>}</li>
-          ))}
-        </ul>
+        <input
+          type="text"
+          placeholder="입력한 후 엔터"
+          value={text}
+          onChange={onChange}
+          onKeyUp={onKeyUp}
+        />
       </div>
-      <div>
+      <>
         <ul>
-          {listsData.map((item, key) => (
-            <li key={key} onClick={() => onClick(item.category)}>
-              <button>{item.category}</button>
-            </li>
-          ))}
+          {youtubeData !== ""
+            ? youtubeData.map((item, key) => (
+                <li key={key}>
+                  <img
+                    src={item.snippet.thumbnails.medium.url}
+                    alt={item.snippet.title}
+                  />
+                  <div>
+                    <p>{item.snippet.title}</p>
+                    <p>{item.snippet.channelTitle}</p>
+                    <p>{item.snippet.publishedAt.slice(0, 10)}</p>
+                  </div>
+                </li>
+              ))
+            : null}
         </ul>
-      </div>
+      </>
     </>
   );
 }
