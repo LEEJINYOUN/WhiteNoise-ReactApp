142b723c50bbb423ba6e828c947605fe184c07cc
이진영 <swlsdud1005@naver.com>
2023-05-01 13:13:06 +0900

    로그인 페이지 - 로그인버튼 임시 스타일링

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -33,14 +33,20 @@ export default function Login({ user, setUser }) {
     }
   }, [setUser]);
   return (
-    <div>
+    <section>
       {Object.keys(user).length === 0 ? (
-        <button onClick={googleLogin}>구글 로그인</button>
+        <button
+          className="px-4 py-2 rounded-lg  bg-blue-400 text-white cursor-pointer hover:bg-blue-500 "
+          onClick={googleLogin}
+        >
+          구글 로그인
+        </button>
       ) : (
         <div>
+          <img src={user.photo ?? undefined} alt="user profile" />
           <h3>{user.name}님 환영합니다.</h3>
         </div>
       )}
-    </div>
+    </section>
   );
 }
