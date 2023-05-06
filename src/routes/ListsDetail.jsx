c7610b89f2ea1144f2f352577698fc582853f256
LEE JINYOUNG <swlsdud1005@naver.com>
2023-05-06 18:22:49 +0900

    리스트디테일페이지 - 스타일링

--- src/routes/ListsDetail.jsx
+++ src/routes/ListsDetail.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { AiOutlineHeart } from "react-icons/ai";
 
 export default function ListsDetail() {
   const {
     state: { list },
   } = useLocation();
-  const { title, channelTitle, description } = list.snippet;
+  const { title } = list.snippet;
   return (
-    <div>
+    <section className="flex flex-col w-10/12 md:w-8/12 lg:w-6/12">
       <iframe
+        className="m-auto mt-3 w-full h-[300px] md:h-[350px] lg:h-[400px]"
         id="player"
         type="text/html"
-        width="100%"
-        height="640"
         src={`http://www.youtube.com/embed/${list.id.videoId}`}
         frameBorder="0"
         title={title}
       />
-      <div>
-        <h2>{title}</h2>
-        <h3>{channelTitle}</h3>
-        <pre>{description}</pre>
+      <div className="flex justify-between mt-5">
+        <h2 className="text-sm md:text-base lg:text-lg font-bold">{title}</h2>
+        <button
+          className="text-2xl cursor-pointer"
+          onClick={() => console.log("클릭")}
+        >
+          <AiOutlineHeart />
+        </button>
       </div>
-    </div>
+    </section>
   );
 }
