e5e389bb46bed60cfc4f20be6149599ba91f0b49
LEE JINYOUNG <swlsdud1005@naver.com>
2023-05-10 13:54:52 +0900

    리스트페이지 - 스타일링 1차 수정

--- src/routes/Lists.jsx
+++ src/routes/Lists.jsx
@@ -2,37 +2,36 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { AiOutlineSearch, AiOutlineCloseCircle } from "react-icons/ai";
+import {
+  AiOutlineSearch,
+  AiOutlineCloseCircle,
+  AiOutlineCheck,
+} from "react-icons/ai";
 import { bookmarkCheck } from "../service/user";
 
-const words = ["파도", "풀벌레", "모닥불", "비"];
-const GET_DATA_COUNT = 10;
+const SEARCH_KEYWORD = ["파도", "풀벌레", "모닥불", "비"];
+const GET_DATA_COUNT = 20;
 
 export default function Lists() {
   const [text, setText] = useState("");
   const { keyword } = useParams();
   const youtubeURL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${GET_DATA_COUNT}&q=백색소음 ${text}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`;
   const fakeURL = `/lists/search.json`;
-  const {
-    isLoading,
-    error,
-    data: lists,
-  } = useQuery(["lists", keyword], async () => {
+  const { data: lists } = useQuery(["lists", keyword], async () => {
     return axios.get(fakeURL).then((res) => res.data.items);
   });
   const navigate = useNavigate();
 
   const onSubmit = (e) => {
     e.preventDefault();
-    if (words.includes(text)) {
+    if (SEARCH_KEYWORD.includes(text)) {
       navigate(`/lists/${text}`);
     } else {
       setText("");
-      console.log("잘못된 검색입니다.");
     }
   };
 
-  const [liked, setLiked] = useState(Array(GET_DATA_COUNT).fill(false));
+  const [bookmark, setBookmark] = useState(Array(GET_DATA_COUNT).fill(false));
   const user = JSON.parse(localStorage.getItem("userInfo"));
 
   useEffect(() => {
@@ -42,14 +41,14 @@ export default function Lists() {
   return (
     <section className="w-full flex flex-col max-w-screen-xl">
       <form
-        className="grid gp-10 bg-gray-300 rounded-[10px] p-[3rem]"
+        className="m-auto w-full md:w-10/12 lg:w-8/12 grid gp-10 bg-gray-300 rounded-[10px] p-[3rem]"
         onSubmit={onSubmit}
       >
         <div className="flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg">
           <AiOutlineSearch className="text-[25px] cursor-pointer" />
           <input
             type="text"
-            className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
+            className="bg-transparent font-bold text-blue-500 focus:outline-none w-[100%]"
             placeholder="입력한 후 엔터"
             value={text}
             onChange={(e) => setText(e.target.value)}
@@ -63,13 +62,17 @@ export default function Lists() {
           </button>
         </div>
       </form>
+      <div className="m-auto mt-4 text-gray-400">
+        <span>⁕ 검색가능한 단어 : </span>
+        <span className="text-red-500 font-bold">
+          {SEARCH_KEYWORD.join(", ")}
+        </span>
+      </div>
       <div className="my-5">
-        {isLoading && <p>로딩중...</p>}
-        {error && <p>오류입니다.</p>}
-        {keyword && lists ? (
+        {lists && (
           <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {lists.map((list, key) => (
-              <li className="rounded-lg mx-4 xl:mx-0" key={key}>
+              <li className="rounded-lg mx-4 xl:mx-0 my-2" key={key}>
                 <img
                   className="w-full cursor-pointer rounded-lg hover:rounded-none ease-out duration-300"
                   src={list.snippet.thumbnails.medium.url}
@@ -99,14 +102,14 @@ export default function Lists() {
                       {list.snippet.publishedAt.slice(0, 10)}
                     </p>
                     <button
-                      className="cursor-pointer"
+                      className="cursor-pointer border-2 border-gray-400 rounded-full p-1 hover:bg-gray-400 hover:text-white"
                       onClick={() => {
                         if (localStorage.getItem("userInfo") === null) {
-                          alert("먼저 로그인해주세요.");
+                          alert("로그인 후 이용해주세요.");
                         } else {
-                          let likeArray = [...liked];
-                          likeArray[key] = !likeArray[key];
-                          setLiked(likeArray);
+                          let bookmarkArray = [...bookmark];
+                          bookmarkArray[key] = !bookmarkArray[key];
+                          setBookmark(bookmarkArray);
                           bookmarkCheck({
                             email: user.email,
                             id: user.id,
@@ -119,17 +122,13 @@ export default function Lists() {
                         }
                       }}
                     >
-                      추가
+                      <AiOutlineCheck />
                     </button>
                   </div>
                 </div>
               </li>
             ))}
           </ul>
-        ) : (
-          <p className="flex justify-center font-bold text-xl">
-            검색어를 입력하세요.
-          </p>
         )}
       </div>
     </section>
