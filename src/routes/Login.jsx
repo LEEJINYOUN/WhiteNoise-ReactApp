77e58230a3199dd4acfb9cbd6134ea3d865995e6
이진영 <swlsdud1005@naver.com>
2023-05-14 13:30:51 +0900

    로그인페이지 - 스타일링

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -32,13 +32,14 @@ export default function Login({ user, setUser }) {
       }
     },
   });
-  const btnStyle = "px-4 py-2 rounded-lg font-bold cursor-pointer";
+  const btnStyle =
+    "my-2 px-4 py-2 w-[200px] rounded-lg font-bold cursor-pointer";
   return (
-    <section className="flex flex-col">
+    <section className="w-full h-[90vh]">
       {Object.keys(user).length === 0 && (
-        <>
+        <div className="w-[200px] h-auto absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
           <button
-            className={`${btnStyle} bg-blue-400 text-white hover:bg-blue-500 `}
+            className={`${btnStyle} bg-blue-400 text-white hover:bg-blue-500`}
             onClick={googleLogin}
           >
             구글 로그인
@@ -46,7 +47,7 @@ export default function Login({ user, setUser }) {
           <button className={`${btnStyle} bg-yellow-400 hover:bg-yellow-500`}>
             <Link to={KAKAO_AUTH_URL}>카카오 로그인</Link>
           </button>
-        </>
+        </div>
       )}
     </section>
   );
